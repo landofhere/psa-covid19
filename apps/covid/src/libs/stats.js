686eee9d69b2eb6d6c2e310047ebd4ae0261f2c4
Barry Low <bearcanrun@gmail.com>
2020-04-03 17:42:12 -0700

    fix(covid): percentage change calculations

--- apps/covid/src/libs/stats.js
+++ apps/covid/src/libs/stats.js
@@ -2,7 +2,9 @@ export const fatalityRate = d => (d.Deaths / d.Confirmed) * 100
 
 export const recoveryRate = d => (d.Recovered / d.Confirmed) * 100
 
-export const percentChange = (n, c) => (n <= 0 ? 0 : ((c - n) / c) * 100)
+export const percentChange = (n, c) => (n <= 0 ? 0 : (n / c) * 100)
+
+export const percentChangeFromTotal = (n, c) => (n <= 0 ? 0 : ((n - c )/ c) * 100)
 
 export const internalizeCountryName = data => {
   let newData = []
